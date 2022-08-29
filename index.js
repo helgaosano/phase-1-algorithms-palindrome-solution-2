fc04cc93c0f6b41b21432e19336e3fc051728978
tania <taniaosano15@gmail.com>
2022-08-30 00:27:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+for (let i = 0; i < word.length / 2; i++) {
+  const x = word.length - 1-i;
+  if (word[i] !== word[x]) {
+    //if letters don't match, return false
+      return false;
+    }
+  }
+  //if letters match, return true
+  return true;
 }
+isPalindrome("racecar");
 
 /* 
   Add your pseudocode here
+
+  iterate over a string from its first index to its middle index
+  compare the characters 
+  if the letters don't match return false 
+  else return true
 */
 
 /*
